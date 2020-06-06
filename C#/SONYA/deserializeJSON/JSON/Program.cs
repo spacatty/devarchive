48b41cbb722db79af6d05010b753f4fcce456ca8
anxietyangst <bezdnastada@gmail.com>
2020-06-07 02:55:14 +0300

    :tada: + C# JSON LW

---
+++ C#/SONYA/deserializeJSON/JSON/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://api.icndb.com/jokes/random";
+            string response;
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            using (StreamReader sr = new StreamReader(res.GetResponseStream()))
+            {
+                response = sr.ReadToEnd();
+            }
+            valueJSON val = JsonConvert.DeserializeObject<valueJSON>(response);
+            Console.WriteLine("We got a joke for you:\n{0}", val.Value.Joke);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/SONYA/deserializeJSON/JSON/valueJSON.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON
+{
+    public class valueJSON
+    {
+        public jokeJSON Value { get; set; }
+
+    }
+}
